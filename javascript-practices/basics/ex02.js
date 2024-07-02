a44548b66b7c04b6ecc471ba4f45d24caf1fad57
parlyresk <parlyresk325@gmail.com>
2024-07-02 16:56:37 +0900

    javascript

---
+++ javascript-practices/basics/ex02.js
@@ -0,0 +1,20 @@
+/*
+변수와 자료형(data type)
+
+[기본타입, primitive type]
+- undefined
+- number
+- string
+- boolean
+[객체타입]
+- object
+- function
+
+*/
+
+console.log("=== 기본타입(undefined,number,string,boolean) =====");
+
+var u;
+var i=10;
+
+console.log("u:"+typeof(u));

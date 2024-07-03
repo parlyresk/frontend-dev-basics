d4aa705c34dc51bd194c4d8598e9c036b8898681
parlyresk <parlyresk325@gmail.com>
2024-07-03 13:10:10 +0900

    구문

---
+++ javascript-practices/basics/ex05.js
@@ -0,0 +1,37 @@
+/*
+구문(statement)
+1. 자바스크립트의 실행 단위
+2. 구성 요소
+    값, 연산식: 표현식(expression)
+    주석: 구문이지만 실행하지 않음
+    예약어: if, else, for
+3.  구문의 예
+    if, if ~ else
+    while, do~while, for
+    for~in
+    switch
+4.  공백: 토큰 구분, 무시
+5.  세미콜론
+6.  개행
+*/
+
+// ;(세미콜론)이 하는 역할은 표현식을 표현 구문으로 만들어서 바로 실행하게 끔한다
+
+a='hello world';
+console.log(a)
+
+//1. 개행은 ;(세미콜론)를 대체함
+
+i = 10
+console.log(i)
+
+//2. 상황에 따라서는 개행을 공백 취급해줄 수 있음
+i
+=
+10
++
+30
+
+console.log(i)
+
+

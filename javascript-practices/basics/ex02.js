2bc2692f8b8ebbd07b16f0e2ded4176cf9ac2c29
parlyresk <parlyresk325@gmail.com>
2024-07-03 16:45:14 +0900

    배열

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -23,6 +23,22 @@
 - function
 
  */
+
+/* 
+자바스크립트의 객체를 분류하는 또 다른 방법
+1. 내장객체
+    -자바 스크립트 엔진 내부에 미리 내장(정의)되어있는 객체
+    -Number, Boolean, Date, RegDate, Array, ... : 생성자 함수
+    -parseInt, setTimeout, setInterval, ...     : 일반 함수
+
+2. 호스트 객체 
+    -자바스크립트 엔진이 embed되어 있는 호스트 환경에 있는 객체
+    -Browser 호스트: screen, location, navigator, document(DOM), XmlHttpRequest
+    -Node 호스트: http, os, fs, path, process, ...
+
+3. 사용자 객체
+    - 자바스크립트 엔진이 코드를 실행하면서 생성되는 객체
+*/
 console.log("=== 기본타입(undefined, number, string, boolean) ======");
 var u;      // -> var u = undefined;와 동일함
 var i = 10;
